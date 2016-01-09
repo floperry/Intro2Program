318f6d10148bb0e779d30aae6a88b072657d8492
floperry <fengqingsong90@gmail.com>
2016-01-09 14:28:09 +0800

    change search range of low

--- findCubeRoot.py
+++ findCubeRoot.py
@@ -5,21 +5,17 @@
 x = int(raw_input('Enter a number: '))
 epsilon = 0.01
 numGuesses = 0
-low = 0.0
-high = max(1.0, abs(x))
+low = min(-1.0, x)
+high = max(1.0, x)
 ans = (low + high)/2.0
 
-while abs(ans**3 - abs(x)) >= epsilon:
+while abs(ans**3 - x) >= epsilon:
     print 'low =', low, 'high =', high, 'ans =', ans
     numGuesses += 1
-    if ans**3 < abs(x):
+    if ans**3 < x:
         low = ans
     else:
         high = ans
     ans = (low + high)/2.0
 print 'numGuesses =', numGuesses
-if x > 0:
-    print ans, 'is close to cube root of', x
-else:
-    ans = -ans
-    print ans, 'is close to cube root of', x
+print ans, 'is close to cube root of', x
